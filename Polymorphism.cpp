06218e31534d6dffb0a6d7471a50c8c0cf6824c1
Abdul Moiz <123240900+AbdulMoiz9876@users.noreply.github.com>
2023-01-21 19:02:08 +0500

    Add files via upload

---
+++ Polymorphism.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Geeks
+{
+    public:
+    void fun(int a)
+    {
+        cout << "value of a is " << a << endl;
+    }
+    void fun(double a)
+    {
+        cout << "value of a is " << a << endl;
+    }
+    void fun(int a, int b)
+    {
+        cout << "The value of a and b is " << a << " , " << b << endl;
+    }
+};
+  int main() 
+{
+    Geeks obj1;
+    obj1.fun(10);
+    obj1.fun(6.324);
+    obj1.fun(90 , 24);
+    return 0;
+}
